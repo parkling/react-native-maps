8bce66360ea19edcceda6eb9cf35e2de53ab68d9
chinloong <chinloong91@gmail.com>
2017-04-27 17:43:45 +0800

    Disable polyline onPress event

--- android/src/main/java/com/airbnb/android/react/maps/AirMapPolyline.java
+++ android/src/main/java/com/airbnb/android/react/maps/AirMapPolyline.java
@@ -92,7 +92,7 @@ public class AirMapPolyline extends AirMapFeature {
     @Override
     public void addToMap(GoogleMap map) {
         polyline = map.addPolyline(getPolylineOptions());
-        polyline.setClickable(true);
+        polyline.setClickable(false);
     }
 
     @Override
